992d2fbd1f2e5ee5589defd8cb1a96930f083612
rexbrandy <bailey.sdev@gmail.com>
2024-09-08 11:18:22 +1000

    Added comments

--- src/App.js
+++ src/App.js
@@ -60,28 +60,41 @@ function Board({xIsNext, squares, onPlay, boardNumber, gameToPlay}) {
 
 
 export default function Game() {
-  const [boards, setBoards] = useState(Array(9).fill(Array(9).fill(null)));
+  const [boards, setBoards] = useState(Array(9).fill(Array(9).fill(null))); // boards - this is the position of the game
   /*useState(Array(9).fill({
     'winner': null,
     'squares': Array(9).fill(null),
   }));*/
-  const [gameToPlay, setGameToPlay] = useState(null);
-  const [xIsNext, setXIsNext] = useState(true);
+  const [gameToPlay, setGameToPlay] = useState(null); // next game to play
+  const [xIsNext, setXIsNext] = useState(true); // which players turn
 
   const winner = calculateWinner(boards);
 
-  // handle play
+  // handlePlay()
+  // This function is the main game loop.
+  // This is given to each Board instance to handle the gameplay.
+  // nextSquares = Array(9) - this is the updated position of a smaller board
+  // boardNumber = Int - this is the index of board that was played on
+  // nextHame = Int - this is the index of the square that was clicked, this will be the next board to be played on
   function handlePlay(nextSquares, boardNumber, nextGame) {
+    // If there is a winner end the game
     if (winner) {
       return;
     }
 
+    // Make copy of the boards
+    // This will be used to update the boards in the game
     let nextBoards = boards.slice();
 
+    // In the copy of the game we update the board that was played on
     nextBoards[boardNumber] = nextSquares;
+    // Update the boards for next loop
     setBoards(nextBoards);
+    // Give turn to next player
     setXIsNext(!xIsNext);
 
+    // If the smaller board is won, the next player can play anywhere
+    // Otherwise they must play in the corresponding board
     if (calculateBoardWinner(boards[nextGame])) {
       setGameToPlay(null);
     } else {
